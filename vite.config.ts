0776a938795880ec82d1c845613518d7ab8feeb9
Marlon-WebDeveloper <chavianomarlon@gmail.com>
2024-03-10 01:44:02 -0400

    Arreglo el base del vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: "/aerolab-challenge",
   resolve: {
     alias: [
       {
